233e6920cee087cf8694596f9f110b7328fa8e30
Isaias <izazaiasdossantos.is@gmail.com>
2025-06-08 19:05:55 -0300

    feat: Adicionando função para rederizar um pokemon pela busca do elemento input

--- js/script.js
+++ js/script.js
@@ -2,9 +2,12 @@ const pokemonName = document.querySelector(".pokemon__name");
 const pokemonNumber = document.querySelector(".pokemon__number");
 const pokemonImage = document.querySelector(".pokemon__image");
 
+const form = document.querySelector(".form");
+const input = document.querySelector(".input__search");
+
 const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    `https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`
   );
   const data = await APIResponse.json();
   return data;
@@ -19,6 +22,11 @@ const renderPokemon = async (pokemon) => {
     data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
       "front_default"
     ];
+
+  input.value = "";
 };
 
-renderPokemon("6");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  renderPokemon(input.value);
+});
